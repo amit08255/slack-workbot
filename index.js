65c6c6fb10e26889bc1c5d9d8a98fea1db1ddfd3
amit08255 <amitcute3@gmail.com>
2020-02-08 19:52:23 +0530

    Made message overflow dropdown button functional. Delete message by its response URL when any action from overflow menu is selected. Then send response for that selected action option.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { text } = require('micro');
 const axios = require("axios");
 const { parse } = require('querystring')
-const addTaskForUser = require('./lib/eval')
+const addTaskForUser = require('./lib/add-task')
 
 /*
 Slash command POST request structure --
@@ -23,94 +23,96 @@ Slash command POST request structure --
 */
 
 /*
-Overflow menu POST request structure --
+Overflow menu POST request structure payload contains JSON string --
 
 {
-	"type": "block_actions",
-	"team": {
-		"id": "T3S7Y1SGN",
-		"domain": "schooglink"
-	},
-	"user": {
-		"id": "UE4LKKCHF",
-		"username": "amit",
-		"name": "amit",
-		"team_id": "T3S7Y1SGN"
-	},
-	"api_app_id": "ATDGCATDZ",
-	"token": "EIf4E6oUqGN4keNxczLqijRL",
-	"container": {
-		"type": "message",
-		"message_ts": "1581163218.003400",
-		"channel_id": "GTC999C9Z",
-		"is_ephemeral": false
-	},
-	"trigger_id": "945073497287.128270060566.c0e50fddfc0d3dde0f5d5e87922fbbe7",
-	"channel": {
-		"id": "GTC999C9Z",
-		"name": "privategroup"
-	},
-	"message": {
-		"type": "message",
-		"subtype": "bot_message",
-		"text": "This content can't be displayed.",
-		"ts": "1581163218.003400",
-		"bot_id": "BTT6YM55L",
-		"blocks": [{
-			"type": "section",
-			"block_id": "NxP",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Hey, @all I found a new activity here.",
-				"verbatim": false
-			},
-			"accessory": {
-				"type": "overflow",
-				"options": [{
-					"text": {
-						"type": "plain_text",
-						"text": ":heavy_check_mark: Complete",
-						"emoji": true
-					},
-					"value": "complete"
-				}, {
-					"text": {
-						"type": "plain_text",
-						"text": ":x: Delete",
-						"emoji": true
-					},
-					"value": "delete"
-				}],
-				"action_id": "9yZcT"
-			}
-		}, {
-			"type": "divider",
-			"block_id": "EkBEK"
-		}, {
-			"type": "section",
-			"block_id": "Z7I1",
-			"text": {
-				"type": "mrkdwn",
-				"text": "&gt;&gt;&gt; *<https:\\/\\/app.slack.com\\/team\\/UE4LKKCHF|amit>* is working on:\\n\\nhello dude \\n\\n *Activity Status:*\\n:red_circle: In Progress",
-				"verbatim": false
-			}
-		}]
-	},
-	"response_url": "https:\\/\\/hooks.slack.com\\/actions\\/T3S7Y1SGN\\/933063818081\\/P5oCbWznpwXfHteIwo6J5GlE",
-	"actions": [{
-		"type": "overflow",
-		"action_id": "9yZcT",
-		"block_id": "NxP",
-		"selected_option": {
-			"text": {
-				"type": "plain_text",
-				"text": ":heavy_check_mark: Complete",
-				"emoji": true
-			},
-			"value": "complete"
+	"payload": `{
+		"type": "block_actions",
+		"team": {
+			"id": "T3S7Y1SGN",
+			"domain": "schooglink"
 		},
-		"action_ts": "1581165207.806745"
-	}]
+		"user": {
+			"id": "UE4LKKCHF",
+			"username": "amit",
+			"name": "amit",
+			"team_id": "T3S7Y1SGN"
+		},
+		"api_app_id": "ATDGCATDZ",
+		"token": "EIf4E6oUqGN4keNxczLqijRL",
+		"container": {
+			"type": "message",
+			"message_ts": "1581163218.003400",
+			"channel_id": "GTC999C9Z",
+			"is_ephemeral": false
+		},
+		"trigger_id": "945104753815.128270060566.71684f56c5ae927a4edf2b1426369d73",
+		"channel": {
+			"id": "GTC999C9Z",
+			"name": "privategroup"
+		},
+		"message": {
+			"type": "message",
+			"subtype": "bot_message",
+			"text": "This content can't be displayed.",
+			"ts": "1581163218.003400",
+			"bot_id": "BTT6YM55L",
+			"blocks": [{
+				"type": "section",
+				"block_id": "NxP",
+				"text": {
+					"type": "mrkdwn",
+					"text": "Hey, @all I found a new activity here.",
+					"verbatim": false
+				},
+				"accessory": {
+					"type": "overflow",
+					"options": [{
+						"text": {
+							"type": "plain_text",
+							"text": ":heavy_check_mark: Complete",
+							"emoji": true
+						},
+						"value": "complete"
+					}, {
+						"text": {
+							"type": "plain_text",
+							"text": ":x: Delete",
+							"emoji": true
+						},
+						"value": "delete"
+					}],
+					"action_id": "9yZcT"
+				}
+			}, {
+				"type": "divider",
+				"block_id": "EkBEK"
+			}, {
+				"type": "section",
+				"block_id": "Z7I1",
+				"text": {
+					"type": "mrkdwn",
+					"text": "&gt;&gt;&gt; *<https:\\/\\/app.slack.com\\/team\\/UE4LKKCHF|amit>* is working on:\\n\\nhello dude \\n\\n *Activity Status:*\\n:red_circle: In Progress",
+					"verbatim": false
+				}
+			}]
+		},
+		"response_url": "https:\\/\\/hooks.slack.com\\/actions\\/T3S7Y1SGN\\/933095117169\\/RGhwikmZx6Uk5HL5fYRz7EZQ",
+		"actions": [{
+			"type": "overflow",
+			"action_id": "9yZcT",
+			"block_id": "NxP",
+			"selected_option": {
+				"text": {
+					"type": "plain_text",
+					"text": ":heavy_check_mark: Complete",
+					"emoji": true
+				},
+				"value": "complete"
+			},
+			"action_ts": "1581169759.433681"
+		}]
+	}`
 }
 
 */
@@ -134,13 +136,39 @@ module.exports = async (req, res) => {
   const body = parse(await text(req))
   let result;
 
-  console.log(body);
+  let response = body;
 
-  result = addTaskForUser(body.text, body.user_id, body.user_name);
+  if(body["payload"] !== undefined){
+      response = JSON.parse(body["payload"]);
+  }
 
-  const data = { response_type: "in_channel", blocks: result}
+  if(body["command"] !== undefined){
 
-  const respond = await to(axios.post(body.response_url, data));
+        result = addTaskForUser(body.text, body.user_id, body.user_name);
+
+        const data = { response_type: "in_channel", blocks: result}
+    
+        const respond = await to(axios.post(body.response_url, data)); //send message by response URL
+  }
+  else if(response["actions"] !== undefined){
+
+        const selected_option = response["actions"][0]["selected_option"];
+        const option_value = selected_option["value"];
+
+        const response_url = response["response_url"];
+
+        const data = {delete_original: true};
+
+        const respond = await to(axios.post(response_url, data)); //Delete message by response URL
+
+        if(option_value === "delete"){
+
+        }
+        else if(option_value === "complete"){
+            
+        }
+
+  }
 
   res.status(200).send(null);
 }
